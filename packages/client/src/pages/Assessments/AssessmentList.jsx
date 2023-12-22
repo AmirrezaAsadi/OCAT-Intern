daeac8373b65eae93e47af310f7fdddbc67b5923
amir <5337255+AmirrezaAsadi@users.noreply.github.com>
2023-12-22 16:06:15 -0500

    delete button

--- packages/client/src/pages/Assessments/AssessmentList.jsx
+++ packages/client/src/pages/Assessments/AssessmentList.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
+
 import { useTable } from 'react-table';
+
 import { AssessmentService } from '../../services/AssessmentService';
 
 export const AssessmentList = () => {
+
   const [ assessments, setAssessments ] = useState([]);
 
   useEffect(() => {
@@ -12,61 +15,69 @@ export const AssessmentList = () => {
     fetchAssessments();
   }, []);
 
-  // Define columns for react-table
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: `ID`,
-        accessor: `id`, // accessor is the "key" in the data
-      },
-      {
-        Header: `Name`,
-        accessor: `catName`,
-      },
-      // Add more columns as needed
-      {
-        Header: `Date of Birth`,
-        accessor: `catDateOfBirth`, // Assuming 'dob' is the key for the date of birth in your data
-      },
-      {
-        Header: `Risk Level`,
-        accessor: `riskLevel`, // Assuming 'dob' is the key for the date of birth in your data
-      },
-    ],
-    []
-  );
+  const columns = React.useMemo(() => [
+    {
+      Header: `ID`,
+      accessor: `id`,
+    },
+    {
+      Header: `Name            `,
+      accessor: `catName`,
+    },
+    {
+      Header: `Date`,
+      accessor: `riskLevel`,
+    },
+    {
+      Header: `Score`,
+      accessor: `score`,
+    },
+  ], []);
 
-  // Use the useTable Hook to send the columns and data to build the table
   const {
     getTableBodyProps,
     getTableProps,
     headerGroups,
     prepareRow,
     rows,
-  } = useTable({ columns, data: assessments });
+  } = useTable({
+    columns,
+    data: assessments,
+  });
 
-  // Render the UI for your table
   return (
-    <div>
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map(headerGroup =>
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column =>
-                <th {...column.getHeaderProps()}>{column.render(`Header`)}</th>)}
-            </tr>)}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map(row => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => <td {...cell.getCellProps()}>{cell.render(`Cell`)}</td>)}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup =>
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column =>
+              <th {...column.getHeaderProps()}>{column.render(`Header`)}</th>)}
+          </tr>)}
+      </thead>
+
+      <tbody {...getTableBodyProps()}>
+        {rows.map(row => {
+          prepareRow(row);
+
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell =>
+                <td {...cell.getCellProps()}>{cell.render(`Cell`)}</td>)}
+
+              <td>
+                <button onClick={() => handleDelete(row.original.id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          );
+
+          function handleDelete(id) {
+            // Call API to delete assessment by id
+          }
+
+        })}
+      </tbody>
+    </table>
   );
 };
